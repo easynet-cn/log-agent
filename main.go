3dfa5e75f562f541cb5171a69e57c84cb4f0c459
liangtianyu <124244236@qq.com>
2024-01-11 15:52:23 +0800

    设置时区

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 
+	"github.com/golang-module/carbon/v2"
 	_ "github.com/mattn/go-sqlite3"
 
 	"github.com/easynet-cn/log-agent/configuration"
@@ -21,6 +23,9 @@ func main() {
 	object.InitDb(configuration.Config)
 	repository.InitRepository(object.GetDB())
 	object.InitProducer(configuration.Config)
+
+	time.LoadLocation(configuration.Config.GetString("time-zone"))
+	carbon.SetTimezone(configuration.Config.GetString("time-zone"))
 
 	go object.InitWatch(configuration.Config)
 
